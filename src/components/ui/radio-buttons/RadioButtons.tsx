e928ea58b53eddf17bac7f4d34dc480a8a2ee17a
rivoulet <177889548+rivoulet@users.noreply.github.com>
2024-08-21 15:15:54 -0700

    Fix RadioButtons input and styling

--- src/components/ui/radio-buttons/RadioButtons.tsx
+++ src/components/ui/radio-buttons/RadioButtons.tsx
@@ -33,48 +33,53 @@ export function RadioButtons({
                     "--radio-buttons-selected": selected,
                 } as CSSProperties
             }
-            onFocus={
-                hasKeyboardControls
-                    ? undefined
-                    : (e) => {
-                          if (!(e.relatedTarget instanceof HTMLElement)) return;
-                          e.relatedTarget.focus();
-                      }
-            }
         >
             <div className="radio-buttons__highlight" />
-            {...buttons.map(({ content, className = "", isDisabled }, i) => (
-                <label
-                    className={
-                        "radio-buttons__button" +
-                        (isDisabled ? " radio-buttons__button--disabled" : "")
-                    }
-                >
-                    <input
-                        className="radio-buttons__button__input"
-                        type="radio"
-                        name={name}
-                        checked={selected === i}
-                        disabled={isDisabled}
-                        readOnly={!setSelected}
-                        tabIndex={hasKeyboardControls ? undefined : -1}
-                        onChange={
-                            setSelected
-                                ? () => {
-                                      setSelected(i);
-                                  }
-                                : undefined
-                        }
-                    />
-                    <div
-                        className={
-                            className + " radio-buttons__button__content"
-                        }
-                    >
+            {...buttons.map(({ content, className = "", isDisabled }, i) => {
+                const className_ =
+                    className +
+                    " radio-buttons__button" +
+                    (isDisabled ? " radio-buttons__button--disabled" : "");
+                const onSelect = setSelected
+                    ? () => {
+                          setSelected(i);
+                      }
+                    : undefined;
+                const content_ = (
+                    <div className="radio-buttons__button__content">
                         {content}
                     </div>
-                </label>
-            ))}
+                );
+                // Labels' focusin events have relatedTarget === null
+                return hasKeyboardControls ? (
+                    <label className={className_}>
+                        <input
+                            className="radio-buttons__button__input"
+                            type="radio"
+                            name={name}
+                            checked={selected === i}
+                            disabled={isDisabled}
+                            readOnly={!setSelected}
+                            onChange={onSelect}
+                        />
+                        {content_}
+                    </label>
+                ) : (
+                    <button
+                        className={className_}
+                        tabIndex={-1}
+                        disabled={isDisabled}
+                        onClick={onSelect}
+                        onFocus={(e) => {
+                            if (!(e.relatedTarget instanceof HTMLElement))
+                                return;
+                            e.relatedTarget.focus();
+                        }}
+                    >
+                        {content_}
+                    </button>
+                );
+            })}
         </div>
     );
 }
